b891a66611f8bdd3e0102f1a83f060982fdcb91f
Kevin MIller <kevin@driftabout.com>
2017-11-14 13:21:30 -0800

    numbers cleaned and working

--- app.js
+++ app.js
@@ -2,48 +2,32 @@
 
 var numbers = [];
 var numbersLength = 300;
-var searchCount = 1;
 
 for (var i = 1; i <= numbersLength; i++){
   numbers.push(i);
 }
 
-//console.log('numbers', numbers);
-
-searchNumbers( numbers, 231);
-
+searchNumbers( numbers, 301);
 
 function searchNumbers(arr, val) {
   var searchArray = arr;
   var min = 0;
-  var max = searchArray.length - 1;
-  //var previousMax = max;
-  //half the array length used to split the search in two pieces
-  //max = Math.ceil(max / 2);
-  //check each half looking for the value
-  //if not there, check other half
-  //if there, half again until only one value is left
+  var max = arr.length - 1;
   while (searchArray.length > 1 ){
-    console.log('searchCount: ', searchCount++);
-    //half the array length used to split the search in two pieces
-    //max = Math.ceil(max / 2);
-    console.log('min: ', min);
-    console.log('max: ', max);
+    //reset the search array to part of the original array
     searchArray = arr.slice(min, max);
     if (searchArray.includes(val)){
-      console.log('searchArray.includes: ', searchArray, 'value: ', val );
-    //  max = searchArray.length - 1;
+      //save the max value as a variable, if the value was not in this slice
+      //use the previous max to reset the array in the next search
       previousMax = max;
-      //half the array length used to split the search in two pieces
-      //max = Math.ceil(max / 2);
+      //since min and max are references to the original array
+      //caluculate the mid point of the original array to split the search in two pieces
       max = Math.ceil((max - min) / 2) + min;
     } else {
-      console.log('searchArray does not include: ', searchArray, 'value: ', val );
+      //since the value was not in the first half of the slice
+      //reset the min to the max to search the second half
       min = max;
       max = previousMax;
-      console.log('not include min: ', min);
-      console.log('not include max: ', max);
-      //max = arr.length - 1;
     }
   }
   console.log('searchArray final: ', searchArray[0]);
